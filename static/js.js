76b9c156190701b20169bb31b9ba3bd84f109964
Andrea Barrandeguy <abarrandeguy87@hotmail.com>
2024-09-19 12:33:50 -0300

    Fixed desktop view width

--- static/js.js
+++ static/js.js
@@ -73,3 +73,20 @@ document.getElementById('upload').addEventListener('change', function () {
         window.location.href = 'home.html'; // Change as needed
     }
 });
+
+//Photo saved
+document.querySelectorAll('.bookmark-toggle').forEach(toggle => {
+    toggle.addEventListener('change', function () {
+        if (this.checked) {
+            const popup = document.getElementById('popup');
+            popup.style.display = 'block'; // Show the popup
+            popup.style.opacity = '1'; // Ensure it's visible
+            setTimeout(() => {
+                popup.style.opacity = '0'; // Fade out
+                setTimeout(() => {
+                    popup.style.display = 'none'; // Hide it after fade out
+                }, 500); // Match this time to the CSS transition
+            }, 3000); // Keep it visible for 3 seconds
+        }
+    });
+});
